5387983add14d6eccb2bd64b746e62660c3734af
VitalBn <vitaliy1byrdin@gmail.com>
2024-04-20 19:45:45 +0300

    REQ-01: Base authentication

---
+++ src/main/java/com/magicvet/Main.java
@@ -0,0 +1,34 @@
+package main.java.com.magicvet;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static final String PASSWORD = "default";
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        run();
+    }
+
+
+    static void run() {
+        boolean accepted = false;
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Password: ");
+            String input = Main.SCANNER.nextLine();
+
+            if (PASSWORD.equals(input)) {
+                accepted = true;
+                break;
+            } else {
+                System.out.println("Access denied. Please check your password.");
+            }
+        }
+
+        System.out.println(accepted ? "Welcome to the Magic Vet!" : "Application has been blocked.");
+
+    }
+}
+
+
