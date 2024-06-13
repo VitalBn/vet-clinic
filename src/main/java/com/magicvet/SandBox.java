c0c10379c79cb2333a28e937085f7e5ed6411366
VitalBn <vitaliy1byrdin@gmail.com>
2024-06-13 12:38:17 +0300

    ReFactoring: Replace lambda expressions instead of anonymous class in SandBox.java.

--- src/main/java/com/magicvet/SandBox.java
+++ src/main/java/com/magicvet/SandBox.java
@@ -6,7 +6,6 @@ import model.Dog;
 import model.Pet;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class SandBox {
 
@@ -20,13 +19,7 @@ public class SandBox {
                 new Dog(Dog.Size.S)
         };
 
-        Arrays.sort(dogs, new Comparator<Dog>() {
-            @Override
-            public int compare(Dog o1, Dog o2) {
-                return o1.getSize().getValue() - o2.getSize().getValue();
-            }
-        });
-
+        Arrays.sort(dogs, (dog1, dog2) -> dog1.getSize().getValue() - dog2.getSize().getValue());
 
         for (Dog dog : dogs) {
             System.out.println(dog.getSize());
@@ -40,41 +33,26 @@ public class SandBox {
                 new Cat("Riki", "4", Pet.Health.GOOD),
         };
 
-        Arrays.sort(cats, new Comparator<Pet>() {
-            @Override
-            public int compare(Pet pet1, Pet pet2) {
-                return pet1.getName().compareTo(pet2.getName());
-            }
-        });
+        Arrays.sort(cats, (cat1, cat2) -> cat1.getName().compareTo(cat2.getName()));
 
         for (Cat cat : cats) {
             System.out.println(cat.getName());
         }
 
-        Arrays.sort(cats, new Comparator<Pet>() {
-            @Override
-            public int compare(Pet pet1, Pet pet2) {
-                return Integer.parseInt(pet1.getAge()) - Integer.parseInt(pet2.getAge());
-            }
-        });
+        Arrays.sort(cats, (cat1, cat2) -> Integer.parseInt(cat1.getAge()) - Integer.parseInt(cat2.getAge()));
 
         for (Cat cat : cats) {
             System.out.println(cat.getAge());
         }
 
-        Arrays.sort(cats, new Comparator<Pet>() {
-            @Override
-            public int compare(Pet pet1, Pet pet2) {
-                return pet1.getHealth().getValue() - pet2.getHealth().getValue();
-            }
-        });
+        Arrays.sort(cats, (cat1, cat2) -> cat1.getHealth().getValue() - cat2.getHealth().getValue());
 
         for (Cat cat : cats) {
             System.out.println(cat.getName() + " has " + cat.getHealth() + " health.");
         }
-        System.out.println(new Cat());
+       /* System.out.println(new Cat());
         System.out.println(new Dog());
-        System.out.println(new Client());
+        System.out.println(new Client());*/
     }
 
 }
